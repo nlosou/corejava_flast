70611dab0c660e7b73a002a95ac8c3d11e6920db
nlosou <2128819713@qq.com>
2025-01-03 21:32:46 +0800

    1.7

---
+++ C1/src/C1_7.java
@@ -0,0 +1,10 @@
+import jdk.jfr.Unsigned;
+
+import java.util.Scanner;
+
+public class C1_7 {
+    public  static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+    }
+}
