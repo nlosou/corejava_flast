eee79dbc44f57d98c984b47a044cba32c2f85772
nlosou <2128819713@qq.com>
2025-01-05 15:58:11 +0800

    update

--- C2/src/C2_5.java
+++ C2/src/C2_5.java
@@ -1,13 +1,33 @@
+/**
+ * The type Point.
+ */
 record Point(int x, int y) {
-    // 平移方法
+    /**
+     * Translate point.
+     *
+     * @param dx the dx
+     * @param dy the dy
+     * @return the point
+     */
+// 平移方法
     public Point translate(int dx, int dy) {
         return new Point(x + dx, y + dy);
     }
 
-    // 缩放方法
+    /**
+     * Scale point.
+     *
+     * @param factor the factor
+     * @return the point
+     */
+// 缩放方法
     public Point scale(double factor) {
         return new Point((int) (x * factor), (int) (y * factor));
     }
+
+    /**
+     * Function.
+     */
     public void function()
     {
         System.out.printf("%d %d",x,y);
@@ -15,7 +35,15 @@ record Point(int x, int y) {
 
 }
 
+/**
+ * The type C 2 5.
+ */
 public class C2_5 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args)
     {
         Point p=new Point(3,4).translate(1,3).scale(0.5);
--- C2/src/C2_6.java
+++ C2/src/C2_6.java
@@ -1,4 +1,12 @@
+/**
+ * The type C 2 6.
+ */
 public class C2_6 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args)
     {
         Point_class a=new Point_class(1,1);
@@ -7,36 +15,78 @@ public class C2_6 {
         System.out.printf("%d %d",a.getX(),a.getY());
     }
 }
+
+/**
+ * The type Point class.
+ */
 class Point_class {
     private int x;
     private int y;
 
-    public Point_class(int x,int y)
+
+    /**
+     * Instantiates a new Point class.
+     *
+     * @param x the x
+     * @param y the y
+     */
+    public Point_class(int x, int y)
     {
         this.x=x;
         this.y=y;
     }
+
+    /**
+     * Function.
+     */
     public void function()
     {
         x=x+1;
         y=y+1;
     }
+
+    /**
+     * Translate.
+     *
+     * @param dx the dx
+     * @param dy the dy
+     */
     public void translate(int dx,int dy)
     {
         x=x+dx;
         y=y+dy;
     }
+
+    /**
+     * Scale.
+     *
+     * @param s the s
+     */
     public void scale(double s)
     {
         x= (int) (x*s);
         y=(int)(y*s);
     }
+
+    /**
+     * Gets x.
+     *
+     * @return the x
+     */
     public int getX()
     {
         return this.x;
     }
+
+    /**
+     * Gets y.
+     *
+     * @return the y
+     */
     public int getY()
     {
         return  this.y;
     }
+
+
 }
