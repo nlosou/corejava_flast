186d1d20056ea0133b83675107f4e845d7494e03
nlosou <2128819713@qq.com>
2025-01-08 16:02:09 +0800

    3.1

--- C3/src/C3_1.java
+++ C3/src/C3_1.java
@@ -6,45 +6,56 @@
 
 public class C3_1 {
 
+    public static void main(String[] args) {
+// 创建 Employee 对象
+        text1.Employee emp1 = new text1.Employee(50000);
+        text1.Employee emp2 = new text1.Employee(60000);
+        text1.Employee emp3 = new text1.Employee(70000);
+
+        // 创建 Measurable 对象数组
+        text1.Measurable[] employees = {emp1, emp2, emp3};
+
+        // 计算平均值
+        double averageSalary = emp1.average(employees);
+        System.out.println("Average Salary: " + averageSalary);
+    }
+}
+
+
+class  text1{
+
+
     @FunctionalInterface
-    public interface Measure {
+    public interface Measurable{
         double getMeasure();
     }
 
-    public class Employee implements Measure {
-        private double salary; // 假设员工的度量值是工资
-
-        public Employee(double salary) {
-            this.salary = salary;
+    public static class Employee implements Measurable{
+        private double salay;
+        public Employee(double salay)
+        {
+            this.salay=salay;
         }
-
         @Override
-        public double getMeasure() {
-            return this.salary; // 返回员工的工资作为度量值
+        public double getMeasure()
+        {
+            return salay;
         }
-
-        public double average(Measure[] objects) {
+         public double average(Measurable[] objects)
+        {
             if (objects == null || objects.length == 0) {
                 throw new IllegalArgumentException("Array must not be null or empty");
             }
 
             double sum = 0.0;
-            for (Measure obj : objects) {
+            for (Measurable obj : objects) {
+                //System.out.println(obj);
+                System.out.println(obj.getMeasure());
                 sum += obj.getMeasure(); // 累加每个对象的度量值
             }
             return sum / objects.length; // 计算平均值
         }
     }
 
-    public static void main(String[] args) {
-        C3_1 c3_1 = new C3_1();
-        C3_1.Employee emp1 = c3_1.new Employee(50000);
-        C3_1.Employee emp2 = c3_1.new Employee(60000);
-        C3_1.Employee emp3 = c3_1.new Employee(70000);
 
-        C3_1.Employee[] employees = {emp1, emp2, emp3};
-
-        double averageSalary = emp1.average(employees);
-        System.out.println("Average Salary: " + averageSalary);
-    }
 }
