fc9db2a9591921a88e8fa48d0681263f495dbc53
nlosou <2128819713@qq.com>
2025-01-15 14:37:44 +0800

    4.2

---
+++ C4/src/C4_2.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025.
+ * @author 漾木茶
+ *
+ */
+
+public class C4_2 {
+    public static void main(String[] args)
+    {
+        Point_2 a= new Point_2(5,6);
+        System.out.println(a.getY());
+        LabeledPoint_2 b= new LabeledPoint_2("ajaja",66,33);
+        System.out.print(b.getLabel());
+        System.out.println(b.getY());
+    }
+}
+class Point_2{
+    double x;
+    double y;
+    public Point_2(double x, double y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
+
+class LabeledPoint_2 extends Point{
+    private String label;
+    public LabeledPoint_2(String label,double x,double y)
+    {
+        super(x,y);
+        this.label=label;
+
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
