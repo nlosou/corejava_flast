ae564fbea8199d9f52ee5680bd0914d075eeca39
nlosou <2128819713@qq.com>
2025-01-15 13:43:32 +0800

    3.12 *

--- C3/src/C3_12.java
+++ C3/src/C3_12.java
@@ -8,25 +8,25 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class C3_11 {
+public class C3_12 {
 
     public static List<File> getSubdirectoriesLambda(File directory) {
         if (directory == null || !directory.isDirectory()) {
             throw new IllegalArgumentException("Provided file is not a directory");
         }
-        List<File> subdirectories = new ArrayList<>();//对象实例
-        // 使用Lambda表达式作为FileFilter
-        File[] files = directory.listFiles(file -> file.isDirectory());
-        if (files != null) {
-            for (File file : files) {
-                subdirectories.add(file);
+        List<File> subdirectories = new ArrayList<>();
+        // 使用Lambda表达式作为FilenameFilter
+        String[] fileNames = directory.list((File dir, String name) -> new File(dir, name).isDirectory());
+        if (fileNames != null) {
+            for (String name : fileNames) {
+                subdirectories.add(new File(directory, name));
             }
         }
         return subdirectories;
     }
 
     public static void main(String[] args) {
-        File dir = new File("/home/noser/java");
+        File dir = new File("/home/noser/java/C3/src/");
         List<File> subdirectories = getSubdirectoriesLambda(dir);
         for (File subdir : subdirectories) {
             System.out.println(subdir.getAbsolutePath());
