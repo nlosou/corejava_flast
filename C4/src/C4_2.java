b3fa442a187c60f2d6e95e0ae3262f667b06c607
nlosou <2128819713@qq.com>
2025-01-16 13:25:08 +0800

    4.2

--- C4/src/C4_2.java
+++ C4/src/C4_2.java
@@ -4,24 +4,26 @@
  *
  */
 
-public class C4_2 {
-    public static void main(String[] args)
-    {
-        Point_2 a= new Point_2(5,6);
-        System.out.println(a.getY());
-        System.out.println(a.toString());
-        LabeledPoint_2 b= new LabeledPoint_2("ajaja",66,33);
-        System.out.println(b.toString());
+import java.util.Objects;
 
+public class C4_2 {
+    public static void main(String[] args) {
+        Point_2 a = new Point_2(5, 6);
+        System.out.println(a.getY());  // 输出: 6.0
+        System.out.println(a.toString());  // 输出: Point_2@<hashcode>改写了 (5.0, 6.0)
+
+        LabeledPoint_2 b = new LabeledPoint_2("ajaja", 66, 33);
+        System.out.println(b.toString());  // 输出: LabeledPoint_2@<hashcode>改写了 (ajaja, 66.0, 33.0)
     }
 }
-class Point_2{
+
+class Point_2 {
     double x;
     double y;
-    public Point_2(double x, double y)
-    {
-        this.x=x;
-        this.y=y;
+
+    public Point_2(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
     public double getX() {
@@ -31,24 +33,54 @@ class Point_2{
     public double getY() {
         return y;
     }
-    public String toString()
-    {
-        return super.toString()+"改写了";
+
+    @Override
+    public String toString() {
+        return super.toString() + " (" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point_2 point_2 = (Point_2) o;
+        return Double.compare(point_2.x, x) == 0 &&
+                Double.compare(point_2.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
 
-class LabeledPoint_2 extends Point{
+class LabeledPoint_2 extends Point_2 {
     private String label;
-    public LabeledPoint_2(String label,double x,double y)
-    {
-        super(x,y);
-        this.label=label;
 
+    public LabeledPoint_2(String label, double x, double y) {
+        super(x, y);
+        this.label = label;
     }
+
     public String getLabel() {
         return label;
     }
+
+    @Override
     public String toString() {
-        return super.toString() + " (" + label + ", " + x + ", " + y + ")";
+        return super.toString() + " (" + label + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LabeledPoint_2 that = (LabeledPoint_2) o;
+        return super.equals(o) && Objects.equals(label, that.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), label);
     }
 }
